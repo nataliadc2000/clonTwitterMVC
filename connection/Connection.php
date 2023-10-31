3ffd3e0a1380e676a269893dac1e9bacb2f8565c
Natalia Domínguez <ndcarbajosa@gmail.com>
2023-10-31 19:24:36 +0100

    creacion de  las carpetas principales del proyecto en el modelo MVC

---
+++ connection/Connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+function connection($host, $user, $pass, $bd) {
+    return new PDO("mysql:host=$host;dbname=$bd", $user, $pass);
+}
+
+try {
+    $host = "localhost:3306";
+    $user = "root";
+    $pass = "root";
+
+    $bd = "northwind";
+
+    $pdo = connection($host, $user, $pass, $bd);
+}  catch (PDOException $e) {
+    header("Location: ../errors/Error.php");
+}
+
+?>
